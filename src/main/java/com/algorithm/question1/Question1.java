3dda9d9017cfcf40a5c25872d7d3241deef15bd3
Samdasoo(박성우) <xungwoo@coupang.com>
2016-05-01 20:58:01 +0900

    samdasoo Q1

---
+++ src/main/java/com/algorithm/question1/Question1.java
@@ -0,0 +1,26 @@
+package com.algorithm.question1;
+
+import java.util.Stack;
+
+public class Question1 {
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+        String source = "Algorithm are awesome!";
+        for (String input : source.split(" ")) {
+            stack.push(input);
+        }
+
+        while(stack.iterator().hasNext())
+            System.out.print(stack.pop() + " ");
+
+
+
+
+    }
+}
